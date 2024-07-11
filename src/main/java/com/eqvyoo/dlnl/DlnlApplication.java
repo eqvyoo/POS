a656df809c2115307dc8daff1fe14c7ff430de5b
eqvyoo <ys07106@gmail.com>
2024-07-11 23:43:22 +0900

    기본 환경 세팅

---
+++ src/main/java/com/eqvyoo/dlnl/DlnlApplication.java
@@ -0,0 +1,13 @@
+package com.eqvyoo.dlnl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlnlApplication.class, args);
+    }
+
+}
